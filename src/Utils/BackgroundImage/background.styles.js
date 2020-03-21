333679bc7e3b5c1df53f971537da1369dab9a5ae
Ridhwan Adagiri <ibrahimridwan47@gmail.com>
2020-03-21 10:10:08 -1100

    changed the height of the background

--- src/Utils/BackgroundImage/background.styles.js
+++ src/Utils/BackgroundImage/background.styles.js
@@ -58,7 +58,7 @@ top: 0;
   background: #09203f;
   background: -webkit-linear-gradient(to left, #537895, #0c356b71);
   width: 100%;
-  height: 100vh;
+  min-height: 100vh;
 `;
 
 export const Circles = styled.ul`
